9bbb9f5486b5fff6d43e3304e1ac4bbcb6fdce43
ronaldfernandotrejo@gmail.com <ronaldfernandotrejo@gmail.com>
2025-05-20 20:01:33 -0600

    feat: add CreateUserController with create method to store new users

---
+++ app/Http/Controllers/CreateUserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class CreateUserController extends Controller
+{
+    public function create(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'phone_number' => 'required|string|max:15',
+        ]);
+
+        // Create the user
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+            'phone_number' => $validatedData['phone_number'],
+        ]);
+
+        return response()->json(['message' => 'User created successfully', 'user' => $user], 201);
+    }
+}
